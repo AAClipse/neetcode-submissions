ad4032e29267c0bc6d1ae63d9b516f3225acedee
AAClipse <168186783+AAClipse@users.noreply.github.com>
2026-08-27 11:32:07 +0300

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public bool SearchMatrix(int[][] matrix, int target) {
+        int left = 0;
+        int right = matrix.Length * matrix[0].Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            int row = mid / matrix[0].Length;
+            int col = mid % matrix[0].Length;
+
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] < target){
+                left = mid + 1;
+            } 
+            else{
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+}
