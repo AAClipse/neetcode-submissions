a5446108636badb6e8a2b232d1c7856ce244f2c3
AAClipse <168186783+AAClipse@users.noreply.github.com>
2026-08-27 17:28:06 +0300

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        left = 0
+        right = len(matrix) * len(matrix[0]) - 1
+        while left <= right:
+            mid = left + (int)((right - left) / 2)
+            row = (int)(mid / len(matrix[0]))
+            col = (int)(mid % len(matrix[0]))
+            if matrix[row][col] == target:
+                return True
+            elif  matrix[row][col] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False
+
+            
+        
