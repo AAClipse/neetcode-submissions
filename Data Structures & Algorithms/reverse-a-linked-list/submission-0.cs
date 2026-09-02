3b871bcda91bc1d5a7084cf7e6074844d7e8c182
AAClipse <168186783+AAClipse@users.noreply.github.com>
2026-09-02 19:53:52 +0300

    Add: reverse-a-linked-list - submission-0

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-0.cs
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+ 
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode cur = head;
+        ListNode next_node;
+        ListNode prev = null;
+        while(cur != null){
+            next_node = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next_node;
+        }
+        return prev;
+    }
+}
